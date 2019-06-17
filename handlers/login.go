e0232cf2631f83f66f64c25be0f139c43ac948c0
thailandmatt <31248374+thailandmatt@users.noreply.github.com>
2019-06-17 13:48:13 -0600

    updated with initial cache pull

--- handlers/login.go
+++ handlers/login.go
@@ -1,15 +1,40 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/byuoitav/pi-time/helpers"
 	"github.com/byuoitav/pi-time/socket"
 	"github.com/labstack/echo"
 )
 
+//LogInUser will authenticate a user, upgrade to websocket, and return the timesheet and offline mode to the web socket
 func LogInUser(context echo.Context) error {
-
 	//do something to authenticate the user, then upgrade to a websocket
 
-	socket.ServeWebsocket(context.Response().Writer, context.Request())
-	//return context.JSON(http.StatusOK, "")
+	//get the id
+	byuID := context.Param("id")
+
+	//get the timesheet for this guy
+	timesheet, isOffline, err := helpers.GetTimesheet(byuID)
+
+	if err != nil {
+		return context.String(http.StatusForbidden, "invalid BYU ID")
+	}
+
+	//upgrade the connection to a websocket
+	webSocketClient := socket.ServeWebsocket(context.Response().Writer, context.Request())
+
+	//store the websocket connection in a map so we can get to it later for that employee id
+	socket.AddConnection(byuID, webSocketClient)
+
+	//send the timesheet down the web socket
+	socket.SendMessageToClient(byuID, "timesheet", timesheet)
+
+	//if offline, send an offline message down the web socket
+	if isOffline {
+		socket.SendMessageToClient(byuID, "offline-mode", true)
+	}
+
 	return nil
 }
--- helpers/ytimeapi.go
+++ helpers/ytimeapi.go
@@ -1,13 +1,42 @@
 package helpers
 
-func GetTimesheet(byuid string) error {
+import (
+	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/pi-time/structs"
+	"github.com/byuoitav/wso2services/wso2requests"
+)
+
+//GetTimesheet returns a timesheet, a bool if the timesheet was returned in offline mode (from cache), and possible error
+func GetTimesheet(byuid string) (structs.Timesheet, bool, error) {
+
+	var timesheet structs.Timesheet
+
 	err :=
-		wso2services.MakeWSO2Request("GET", "https://api.byu.edu:443/domains/erp/hr/timesheet/v1/"+byuid, "", timesheet)
+		wso2requests.MakeWSO2Request("GET", "https://api.byu.edu:443/domains/erp/hr/timesheet/v1/"+byuid, "", &timesheet)
 
 	if err != nil {
-		if httpResponse.StatusCode/100 == 4 {
-			//this means they don't have jobs to punch in
+		log.L.Debugf("Error when making WSO2 request to get timesheet %v", err)
+
+		//look in the cache
+		employeeRecord, innerErr := GetEmployeeFromCache(byuid)
+		if innerErr != nil {
+			//not found
+			log.L.Debugf("No cached timesheet found")
+			return timesheet, false, err
+		} else {
+			log.L.Debugf("Cached timesheet found")
+			timesheet := structs.Timesheet{
+				PersonName:           employeeRecord.Name,
+				WeeklyTotal:          "--:--",
+				PeriodTotal:          "--:--",
+				InternationalMessage: "",
+				Jobs:                 employeeRecord.Jobs,
+			}
+
+			return timesheet, true, nil
+
 		}
 	}
 
+	return timesheet, false, nil
 }
