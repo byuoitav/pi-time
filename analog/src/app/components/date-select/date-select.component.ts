4753bbf8a383e09285c209b0284964f8e9fa01a5
jpw547 <jwerner547@gmail.com>
2019-07-09 15:37:31 -0600

    the calendar date select page should be done

--- analog/src/app/components/date-select/date-select.component.ts
+++ analog/src/app/components/date-select/date-select.component.ts
@@ -11,6 +11,35 @@ import { Employee } from "../../objects";
 })
 export class DateSelectComponent implements OnInit {
   jobIdx: number;
+  today: Date;
+  viewMonth: number;
+  viewYear: number;
+  viewDays: Date[];
+
+  MonthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+
+  DayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
 
   private _emp: BehaviorSubject<Employee>;
   get emp(): Employee {
@@ -34,6 +63,47 @@ export class DateSelectComponent implements OnInit {
 
       console.log("day select job", this.emp.jobs[this.jobIdx]);
     });
+
+    this.today = new Date();
+    this.viewMonth = this.today.getMonth();
+    this.viewYear = this.today.getFullYear();
+
+    this.getViewDays();
+  }
+
+  goBack() {
+    window.history.back();
+  }
+
+  canMoveMonthBack(): boolean {
+    // return (this.viewMonth < this.today.getMonth())
+    return true;
+  }
+
+  canMoveMonthForward(): boolean {
+    return (this.viewMonth < this.today.getMonth())
+  }
+
+  moveMonthBack() {
+    if (this.viewMonth === 0) {
+      this.viewMonth = 11;
+      this.viewYear--;
+    } else {
+      this.viewMonth--;
+    }
+
+    this.getViewDays();
+  }
+
+  moveMonthForward() {
+    if (this.viewMonth === 11) {
+      this.viewMonth = 0;
+      this.viewYear++;
+    } else {
+      this.viewMonth++;
+    }
+
+    this.getViewDays();
   }
 
   selectDay = (idx: number) => {
@@ -44,4 +114,41 @@ export class DateSelectComponent implements OnInit {
     const max = this.emp.jobs[this.jobIdx].days.length - 1;
     this.selectDay(Math.floor(Math.random() * Math.floor(max)));
   };
+
+  getViewDays() {
+    this.viewDays = [];
+    const lastDayOfLastMonth = new Date(this.viewYear, this.viewMonth, 0);
+    const start = lastDayOfLastMonth.getDate() - lastDayOfLastMonth.getDay();
+    const startDate = new Date(this.viewYear, this.viewMonth - 1, start);
+    
+    for (let i = 0; i < 42; i++) {
+      const d = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+      d.setDate(startDate.getDate() + i);
+      this.viewDays.push(d);
+    }
+  }
+
+  dayHasException(day: Date): boolean {
+    const empDay = this.emp.jobs[this.jobIdx].days.find(
+      d => d.time.toDateString() === day.toDateString()
+    );
+
+    if (empDay != null) {
+      return empDay.hasPunchException || empDay.hasWorkOrderException;
+    } else {
+      return false;
+    }
+  }
+
+  dayHasPunch(day: Date): boolean {
+    const empDay = this.emp.jobs[this.jobIdx].days.find(
+      d => d.time.toDateString() === day.toDateString()
+    );
+
+    if (empDay != null) {
+      return empDay.punches.length > 0
+    } else {
+      return false;
+    }
+  }
 }
