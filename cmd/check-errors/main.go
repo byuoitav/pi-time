07056e01287a9cb10cd553d9e7bc0a8fa99f2a1d
Daniel Randall <dgrandall@me.com>
2020-10-09 10:19:39 -0600

    cmd to search for errors

---
+++ cmd/check-errors/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/byuoitav/common/db"
+	"github.com/byuoitav/common/structs"
+	pstructs "github.com/byuoitav/pi-time/structs"
+)
+
+func main() {
+	devs, err := db.GetDB().GetAllDevices()
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	fmt.Printf("deviceID,time,byuID\n")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+
+	for _, dev := range devs {
+		if dev.Type.ID != "TimeClock" {
+			continue
+		}
+
+		wg.Add(1)
+
+		go func(dev structs.Device) {
+			defer wg.Done()
+
+			l := log.New(os.Stderr, dev.ID+" ", log.Flags())
+
+			// get error bucket from this device
+			url := fmt.Sprintf("http://%s:8463/buckets/error/punches", dev.Address)
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+			if err != nil {
+				l.Printf("error building request: %s", err)
+				return
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				l.Printf("error doing request: %s", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				l.Printf("bad status code %v", resp.StatusCode)
+				return
+			}
+
+			punches := struct {
+				Punches []struct {
+					Key   string `json:"Key"`
+					Punch struct {
+						Err   string                      `json:"Err"`
+						Punch pstructs.ClientPunchRequest `json:"Punch"`
+					}
+				} `json:"Punches"`
+			}{}
+
+			if err := json.NewDecoder(resp.Body).Decode(&punches); err != nil {
+				l.Printf("error decoding: %s", err)
+				return
+			}
+
+			for _, punch := range punches.Punches {
+				if time.Since(punch.Punch.Punch.Time) < 24*time.Hour {
+					fmt.Printf("%v,%v,%v\n", dev.ID, punch.Punch.Punch.Time.Format(time.RFC3339), punch.Punch.Punch.BYUID)
+				}
+			}
+		}(dev)
+	}
+
+	wg.Wait()
+}
